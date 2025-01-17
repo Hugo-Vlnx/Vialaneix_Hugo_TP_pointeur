e954b75435d104874895d4d1d754c008dc9623a9
Hugo.V <hugo.vialaneix@gmail.com>
2025-01-17 14:20:21 +0100

    fct pointeur

--- Vialaneix_Hugo_pointeur/pointeur.c
+++ Vialaneix_Hugo_pointeur/pointeur.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-
-
-
-
-int main() {
-	int a = 5;
-	printf("voici : %p\n", &a);
-
-	int *p = &a;
-	printf("voici :%p\n", p);
-
-	printf("voici :%p\n", &p);
-	*p = 7;  
-	printf("voici : %d \n ", a);
-
-}
+//#include <stdio.h>
+//
+//
+//
+//
+//int main() {
+//	int a = 5;
+//	printf("voici : %p\n", &a);
+//
+//	int *p = &a;
+//	printf("voici :%p\n", p);
+//
+//	printf("voici :%p\n", &p);
+//	*p = 7;  
+//	printf("voici : %d \n ", a);
+//
+//}
---
+++ Vialaneix_Hugo_pointeur/pointeurFct.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+void min_max(int tab[6], int size, int* min, int* max)
+{
+    if (size < 1)
+    {
+        return;
+    }
+    *min = tab[0];
+    *max = tab[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (*min > tab[i])
+        {
+            *min = tab[i];
+        }
+        if (*max < tab[i])
+        {
+            *max = tab[i];
+        }
+    }
+}
+
+int main()
+{
+    int min = 0;
+    int max = 0;
+    int tableau[6] = {12, 8 , 2 , -7 , 15 , -2};
+    min_max(tableau, 6, &min, &max);
+    printf("min = % d, max = % d\n ", min, max);
+    return 0;
+}
